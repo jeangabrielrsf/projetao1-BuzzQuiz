7577fea32145e8259ba0ca2f73d10cbcb31532cb
Tadeuvelloso <tadeuvellososilva@gmail.com>
2022-06-29 21:54:08 -0300

    Aplicando scripts na pagina 1

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,29 @@
-function primeiraFuncao () {
-    
+const urlAPI = "https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes";
+let quizes = []; 
+
+const promessa = axios.get(urlAPI);
+promessa.then(processarResposta);
+
+function processarResposta(jogos){
+    console.log(jogos.data);
+    quizes = jogos.data;
+
+    renderizarQuizzes()
 }
+
+function renderizarQuizzes(){
+
+    const ul = document.querySelector('.quizzes');
+
+    for(let i = 0; i < 6; i++){
+
+        ul.innerHTML += `
+            <div class="quizz">
+                <img src="${quizes[i].image}">
+                <div><h3>${quizes[i].title}</h3></div>
+            </div>
+        `
+    }
+}
+
+renderizarQuizzes();
